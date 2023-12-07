a5d24e7a2241c389ea17c8f0194d575286427e38
Jakub Konkol <101362884+Jakub-Konkol@users.noreply.github.com>
2023-12-07 10:50:01 -0500

    Day 6, non-brute forced

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,24 +1,30 @@
+import math
 import re
 
 
-def race_result_calulator(total_time, distance_to_beat):
-    time_spent_charging = 0
-    number_of_wins = 0
+def race_result_calculator(total_time, distance_to_beat):
+    # old brute force method
+    # time_spent_charging = 0
+    # number_of_wins = 0
+    #
+    # while time_spent_charging < total_time:
+    #     time_left_in_race = total_time - time_spent_charging
+    #     boat_speed = time_spent_charging
+    #
+    #     distance_traveled = boat_speed * time_left_in_race
+    #
+    #     if distance_traveled > distance_to_beat:
+    #         number_of_wins += 1
+    #
+    #     print(f"{time_spent_charging/total_time:.2%} completed", end="\r")
+    #
+    #     time_spent_charging += 1
 
-    while time_spent_charging < total_time:
-        time_left_in_race = total_time - time_spent_charging
-        boat_speed = time_spent_charging
+    # non-brute force way
+    start_win = math.ceil((total_time - math.sqrt(total_time ** 2 - 4 * distance_to_beat)) / 2)
+    end_win = math.floor((total_time + math.sqrt(total_time ** 2 - 4 * distance_to_beat)) / 2)
 
-        distance_traveled = boat_speed * time_left_in_race
-
-        if distance_traveled > distance_to_beat:
-            number_of_wins += 1
-
-        print(f"{time_spent_charging/total_time:.2%} completed", end="\r")
-
-        time_spent_charging += 1
-
-    return number_of_wins
+    return end_win - start_win + 1
 
 
 def part1(filename):
@@ -37,9 +43,10 @@ def part1(filename):
     results = 1
 
     for race_time, race_distance in zip(race_times, race_distances):
-        results *= race_result_calulator(race_time, race_distance)
+        results *= race_result_calculator(race_time, race_distance)
 
     print(results)
+
 
 def part2(filename):
     number_finder = re.compile("\d+")
@@ -54,7 +61,7 @@ def part2(filename):
     race_distances = int(''.join(race_distances))
     file.close()
 
-    results = race_result_calulator(race_times, race_distances)
+    results = race_result_calculator(race_times, race_distances)
 
     print(results)
 
